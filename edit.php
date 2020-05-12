097c004c7786e51e2f3c6da48cb9ab6949494773
doniwirawan <doniwirawan166@gmail.com>
2020-05-12 21:10:00 +0800

    commit ke-4

--- edit.php
+++ edit.php
@@ -7,14 +7,16 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="bootstrap\css\bootstrap.css">
+    <link rel="stylesheet" href="fontawesome-5.2.0\css\all.css">
 
     <title>hapus data</title>
   </head>
   <body>
   <div class="row d-flex justify-content-center">
   <div class="col-4 xs-auto justify-content-center border mt-5 p-4 rounded bg-light ">
-    <h1>Edit data mahasiswa</h1>
-    <a href="view.php">KEMBALI</a>
+    <h1 class="text-capitalize text-center mb-3">Edit data mahasiswa</h1>
+    <a href="view.php" class="text-decoration-none"><i class="fas fa-backward mr-2"></i>KEMBALI</a>
 
     <?php
     include'koneksi.php';
@@ -24,7 +26,7 @@
     ?>
 
 
-    <form action="update.php" action="action">
+    <form action="update.php" method="post" class="mt-4">
         <div class="form-group font-weight-bolder">
           <label for="formGroupExampleInput">Nama</label>
           <input type="hidden" name="id" value="<?php echo $d['id']; ?>">
@@ -36,9 +38,8 @@
           <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" name="nim" required value="<?php echo $d['nim']; ?>">
         </div>
         <div class="form-group font-weight-bolder">
-          <label for="formGroupExampleInput2">Alamat</label>
-          <input type="hidden" name="id" value="<?php echo $d['id']; ?>">
-          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input placeholder" name="alamat" required value="<?php echo $d['alamat']; ?>">
+          <label for="exampleFormControlTextarea1">Alamat</label>
+          <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="alamat"  required value="<?php echo $d['alamat']; ?>"></textarea>
         </div>
         
         <button type="submit" class="btn btn-primary">Simpan</button>
@@ -49,12 +50,15 @@
 	}
 	?>
 
-    
+
   </div>
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="jquery-3.3.1\jquery-3.3.1.js"></script>
+    <script src="fontawesome-5.2.0\js\all.js"></script>
+    <script src="bootstrap\js\bootstrap.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
